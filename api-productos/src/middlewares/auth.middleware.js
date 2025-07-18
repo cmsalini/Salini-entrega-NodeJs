d0f0746f8b7cd797e962b2ecd7ff5ec60557557d
cmsalini <salinicamila@gmail.com>
2025-07-18 13:09:41 -0300

    se mejora manejo de errores con token

--- api-productos/src/middlewares/auth.middleware.js
+++ api-productos/src/middlewares/auth.middleware.js
@@ -1,10 +1,18 @@
 import { verifyToken } from '../utils/jwt.js';
 
 export const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'].split(' ')[1];
-    if (!token) return res.sendStatus(401).json({ message: 'Token requerido' });
-    const verificationResult = verifyToken(token);
-    if (!verificationResult.valid) return res.status(403).json({ message: verificationResult.message });
-    req.user = verificationResult.decoded;
-    next();
+  const authHeader = req.headers['authorization'];
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token requerido' });
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Token requerido' });
+  }
+  const verificationResult = verifyToken(token);
+  if (!verificationResult.valid) {
+    return res.status(403).json({ message: verificationResult.message });
+  }
+  req.user = verificationResult.decoded;
+  next();
 };

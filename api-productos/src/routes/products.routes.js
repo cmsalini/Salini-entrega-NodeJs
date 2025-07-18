c3b667bfdbddd53476d0c28b4e8264dd0074c11b
cmsalini <salinicamila@gmail.com>
2025-07-18 13:10:36 -0300

    se agrega proteccion a rutas de post y delete

--- api-productos/src/routes/products.routes.js
+++ api-productos/src/routes/products.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as productsController from '../controllers/products.controller.js';
+import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.get('/', productsController.getProducts);
 router.get('/:codigo', productsController.getProduct);
-router.post('/', productsController.createProduct);
-router.delete('/:codigo', productsController.deleteProduct);
+router.post('/', authenticateToken, productsController.createProduct);
+router.delete('/:codigo', authenticateToken, productsController.deleteProduct);
 
 export default router; 
